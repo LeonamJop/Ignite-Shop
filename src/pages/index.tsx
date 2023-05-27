762d0c266cb84074ad62a9974b73bf620ecc3440
Leonam Silva <leonam.murilo@gmail.com>
2023-05-27 12:42:36 -0300

    Style: Criando estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: "$green500",
-  borderRadius: 4,
-  border: "none",
-  padding: '4px 8px'
-})
+import camiseta1 from '../assets/Shirt/1.png'
+import camiseta2 from '../assets/Shirt/2.png'
+import camiseta3 from '../assets/Shirt/3.png'
 
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt=""/>
+        
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
