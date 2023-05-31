03b0e71357e37befdcac70ec9d279b1a27d971dc
Leonam Silva <leonam.murilo@gmail.com>
2023-05-31 20:29:39 -0300

    Feat: Utilizando SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from "next";
 import Image from "next/image";
 import { HomeContainer, Product } from "../styles/pages/home";
 
@@ -5,7 +6,6 @@ import { useKeenSlider } from 'keen-slider/react';
 import 'keen-slider/keen-slider.min.css';
 
 import Stripe from "stripe";
-import { GetServerSideProps } from "next";
 import { stripe } from "../lib/stripe";
 
 interface HomeProps {
@@ -44,7 +44,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -64,6 +64,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, //Recria página a cada 2 horas
   }
 }
