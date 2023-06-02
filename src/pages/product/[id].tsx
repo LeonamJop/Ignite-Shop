bd978bd75520206a102e229eaa1c4b52faf01819
Leonam Silva <leonam.murilo@gmail.com>
2023-06-01 21:59:08 -0300

    feat: criando tela do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut eius omnis ut. Iste placeat voluptas aliquid. Quam aliquam illo aspernatur praesentium voluptates illum eveniet recusandae quaerat. Repellendus aspernatur et quod.</p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
